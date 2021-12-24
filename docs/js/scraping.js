c1bce0022d2419a7f891f24fc8fdb3c1fbb6824d
bugra cantepe <66384957+bugracntp@users.noreply.github.com>
2021-12-24 12:28:48 +0300

    fix some issiues

--- docs/js/scraping.js
+++ docs/js/scraping.js
@@ -2,39 +2,40 @@ const projectlist = document.querySelector("#projects-list");
 let repos = [];
 console.log("run");
 (async () => {
-    const repApi = await fetch("https://api.github.com/users/bugracntp/repos")
-        .then(response => response.json());
-    repos = repApi.map(repo => {
-        return {
-            name: repo.name,
-            des: repo.description,
-            url: repo.url,
-            lan: repo.language
-        }
-    });
+  const repApi = await fetch(
+    "https://api.github.com/users/bugracntp/repos"
+  ).then((response) => response.json());
+  repos = repApi.map((repo) => {
+    return {
+      name: repo.name,
+      des: repo.description,
+      url: repo.url,
+      lan: repo.language,
+    };
+  });
 
-    for (let i = 0; i < repos.length; i++) {
-        const repo = repos[i];
-        const projectDiv = document.createElement("div");
-        projectDiv.className = "project";
+  for (let i = 0; i < repos.length; i++) {
+    const repo = repos[i];
+    const projectDiv = document.createElement("div");
+    projectDiv.className = "project";
 
-        const aTag = document.createElement("a")
-        aTag.setAttribute("href", repo.url);
+    const aTag = document.createElement("a");
+    aTag.setAttribute("href", repo.url);
 
-        const divContent = document.createElement("div");
-        divContent.className = "col-content";
-
-        divContent.innerHTML = `<h3 class="col-title">${repo.name}</h3>
-                      <p class="des">${repo.des}</p>
-                      <span class="language">${repo.lan}</span>`;
-
-        aTag.appendChild(divContent);
-        projectDiv.appendChild(aTag);
-        projectlist.appendChild(projectDiv);
-
-
+    const divContent = document.createElement("div");
+    divContent.className = "col-content";
+    if (repo.lan !== "null") {
+      divContent.innerHTML = `<h3 class="col-title">${repo.name}</h3>
+                          <p class="des">${repo.des}</p>
+                          <span class="language">${repo.lan}</span>`;
+    } else {
+      divContent.innerHTML = `<h3 class="col-title">${repo.name}</h3>
+        <p class="des">${repo.des}</p>
+        <span class="language"></span>`;
     }
 
-
-
+    aTag.appendChild(divContent);
+    projectDiv.appendChild(aTag);
+    projectlist.appendChild(projectDiv);
+  }
 })();
