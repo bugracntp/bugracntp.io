737095c737178296a0231999314abac1f986e7c7
bugracntp <can123.bugra@gmail.com>
2025-10-13 14:34:21 +0300

    edit(footer): redesing footer

--- packages/portfolio/src/components/Footer/Footer.tsx
+++ packages/portfolio/src/components/Footer/Footer.tsx
@@ -177,25 +177,10 @@ const Footer: React.FC<FooterProps> = ({ data }) => {
               </a>
             ))}
           </div>
-
-          {/* Scroll to Top Button */}
-          <button 
-            onClick={scrollToTop}
-            className={styles.scrollTop}
-            aria-label="Scroll to top"
-          >
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-              <path d="M18 15l-6-6-6 6"/>
-            </svg>
-          </button>
         </div>
 
         {/* Made with love */}
         <div className={styles.madeWith}>
-          <span>Made with</span>
-          <svg className={styles.heart} width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
-          </svg>
           <span>by Buğra Cantepe</span>
         </div>
       </div>
